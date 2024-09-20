f7aa8160b9d04ab6b27ea08056f4b0d38800805f
JustAHuman-xD <65748158+JustAHuman-xD@users.noreply.github.com>
2024-09-20 11:07:45 -0500

    redo pack screen mixin for 1.20.6

--- src/main/java/me/justahuman/pack_presets/mixins/PackScreenMixin.java
+++ src/main/java/me/justahuman/pack_presets/mixins/PackScreenMixin.java
@@ -8,45 +8,47 @@ import net.minecraft.client.gui.screen.pack.PackListWidget;
 import net.minecraft.client.gui.screen.pack.PackScreen;
 import net.minecraft.client.gui.tooltip.Tooltip;
 import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.gui.widget.DirectionalLayoutWidget;
+import net.minecraft.client.gui.widget.ThreePartsLayoutWidget;
+import net.minecraft.client.gui.widget.Widget;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(PackScreen.class)
 public abstract class PackScreenMixin extends Screen {
-    @Shadow private ButtonWidget doneButton;
+    @Unique private static final Text OPEN_PRESETS = Text.translatable("pack_presets.screen.pack.button.open_presets");
+    @Unique private static final Text OPEN_PRESETS_TOOLTIP = Text.translatable("pack_presets.screen.pack.button.open_presets.tooltip");
+    @Unique private static final Text CREATE_PRESET = Text.translatable("pack_presets.screen.pack.button.create_preset");
+    @Unique private static final Text CREATE_PRESET_TOOLTIP = Text.translatable("pack_presets.screen.pack.button.create_preset.tooltip");
     @Shadow private PackListWidget selectedPackList;
 
     protected PackScreenMixin(Text title) {
         super(title);
     }
 
-    @Inject(at = @At("TAIL"), method = "init")
-    public void addWidgets(CallbackInfo ci) {
-        final int x = this.width / 2;
-        final int y = this.height - 48;
-        this.addDrawableChild(ButtonWidget.builder(Text.translatable("pack_presets.screen.pack.button.open_presets"), button -> {
-            this.close();
-            this.client.setScreen(new PackPresetsScreen(self(), PackPresets.getPresetsDir()));
-        }).tooltip(Tooltip.of(Text.translatable("pack_presets.screen.pack.button.open_presets.tooltip")))
-        .dimensions(x - 100 - 2 - 50, y, 100, 20).build());
-
-        if (this.drawables.get(2) instanceof ButtonWidget folderButton) {
-            folderButton.setDimensionsAndPosition(100, 20, x - 50, y);
+    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/ThreePartsLayoutWidget;addFooter(Lnet/minecraft/client/gui/widget/Widget;)Lnet/minecraft/client/gui/widget/Widget;"), method = "init")
+    public <T extends Widget> T addWidgets(ThreePartsLayoutWidget instance, T widget) {
+        if (widget instanceof DirectionalLayoutWidget footer) {
+            footer.add(ButtonWidget.builder(OPEN_PRESETS, button -> {
+                this.close();
+                this.client.setScreen(new PackPresetsScreen(self(), PackPresets.getPresetsDir()));
+            }).tooltip(Tooltip.of(OPEN_PRESETS_TOOLTIP)).build());
         }
 
-        doneButton.setDimensionsAndPosition(100, 20, x + 50 + 2, y);
-
-        this.addDrawableChild(ButtonWidget.builder(Text.translatable("pack_presets.screen.pack.button.create_preset"), button -> {
+        this.addDrawableChild(ButtonWidget.builder(CREATE_PRESET, button -> {
             this.close();
             this.client.setScreen(new CreatePresetScreen(self()));
-        }).tooltip(Tooltip.of(Text.translatable("pack_presets.screen.pack.button.create_preset.tooltip")))
-        .dimensions(selectedPackList.getX() + selectedPackList.getWidth() + 4, selectedPackList.getY(), 75, 20)
-        .build());
+        }).tooltip(Tooltip.of(CREATE_PRESET_TOOLTIP)).dimensions(
+            selectedPackList.getX() + selectedPackList.getWidth() + 4,
+            selectedPackList.getY(),
+            75, 20
+        ).build());
+
+        return instance.addFooter(widget);
     }
 
     @Unique
